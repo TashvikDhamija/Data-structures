f30730c322cc050aa323852c0ecc77d0d819332c
TashvikDhamija <62467270+TashvikDhamija@users.noreply.github.com>
2020-03-21 17:00:11 +0530

    Create insert node at head of LL.cpp

---
+++ insert node at head of LL.cpp
@@ -0,0 +1,15 @@
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
+    SinglyLinkedListNode* newHead = new SinglyLinkedListNode(data);
+    newHead->next = llist;
+    return newHead;
+
+}
